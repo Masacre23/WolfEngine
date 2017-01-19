a1c7604c766f2f21c85b0234a8d01339569adfe0
guillemfp <guillemferre@gmail.com>
2017-01-19 19:41:35 +0100

    Simplification of the low resolution timer

--- WolfEngine/Timer.h
+++ WolfEngine/Timer.h
@@ -3,109 +3,42 @@
 
 #include "SDL/include/SDL_timer.h"
 
-enum TimerState
-{
-	OFF,
-	RUNNING,
-	PAUSED
-};
-
 class Timer
 {
 public:
-	Timer(Uint32 max_time = 0) : max_time(max_time) {};
+	Timer() {};
 	~Timer() {};
 
-	void Start() { Start(max_time); }
-
-	void Start(float max_time) { Start((Uint32)max_time * 1000); }
-
-	void Start(Uint32 max_time)
+	void Start()
 	{
-		if (state == OFF)
-		{
-			start_time = SDL_GetTicks();
-			this->max_time = max_time;
-			state = RUNNING;
-		}
+		start_time = SDL_GetTicks();
+		running = true;
 	}
 
 	void Stop()
 	{
-		start_time = 0;
-		accumulated_time = 0;
-		state = OFF;
+		final_time = SDL_GetTicks();
+		running = false;
 	}
 
-	void Reset()
-	{
-		Stop();
-		Start(max_time);
-	}
+	bool IsRunning() { return running; }
 
-	void Pause()
-	{
-		if (state == RUNNING)
-		{
-			accumulated_time += SDL_GetTicks() - start_time;
-			start_time = 0;
-			state = PAUSED;
-		}
-	}
+	Uint32 GetTimeInMs() const 
+	{ 
+		Uint32 ret;
 
-	void Resume()
-	{
-		if (state == PAUSED)
-		{
-			start_time = SDL_GetTicks();
-			state = RUNNING;
-		}
-	}
-
-	Uint32 GetTimeInMs() const { return accumulated_time + CurrentTime(); }
-
-	float GetTimeInSeconds() const { return ((float) GetTimeInMs()/1000.0f); }
-
-	Uint32 GetCounterInMs() const { return (max_time - GetTimeInMs() >= 0 ? max_time - GetTimeInMs() : 0); }
-
-	int GetCounterInS() const { return (GetCounterInMs() / 1000); }
-
-	TimerState GetState() const { return state; }
-
-	Uint32 GetMaxTimeInMs() const { return max_time; }
-
-	void DoubleMaxTime() { max_time *= 2; }
-
-	bool MaxTimeReached() const
-	{
-		if (max_time > 0)
-		{
-			return (accumulated_time + CurrentTime() >= max_time);
-		}
+		if (running)
+			ret = SDL_GetTicks() - start_time;
 		else
-			return false;
-	}
+			ret = final_time - start_time;
 
-	void SetMaxTime(Uint32 max_time)
-	{
-		this->max_time = max_time;
-		Stop();
-	}
-
-private:
-	Uint32 CurrentTime() const
-	{
-		if (state == PAUSED || state == OFF)
-			return 0;
-		else
-			return SDL_GetTicks() - start_time;
+		return ret; 
 	}
 
 private:
 	Uint32 start_time = 0;
-	Uint32 accumulated_time = 0;
-	Uint32 max_time = 0;
-	TimerState state = OFF;
+	Uint32 final_time = 0;
+	bool running = false;
 };
 
 #endif // !TIMER_H
