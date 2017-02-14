16227e8bccf825e3908ec1bad0ddd7dc4f35db87
estebanag <estarr22@hotmail.com>
2017-02-14 21:39:42 +0100

    Try to change the mesh a vertex pointer.

--- WolfEngine/Model.cpp
+++ WolfEngine/Model.cpp
@@ -19,6 +19,20 @@ Model::~Model()
 void Model::Load(const char * file)
 {
 	scene = aiImportFile(file, aiProcess_PreTransformVertices | aiProcess_FlipUVs);
+
+	for (int i = 0; i < scene->mNumMeshes; ++i)
+	{
+		aiMesh* mesh = scene->mMeshes[i];
+		std::vector<unsigned int> mesh_indices;
+		for (int j = 0; j < mesh->mNumFaces; ++j)
+		{
+			for (int k = 0; k < mesh->mFaces[j].mNumIndices; ++k)
+			{
+				mesh_indices.push_back(mesh->mFaces[j].mIndices[k]);
+			}
+		}
+		indices.push_back(mesh_indices);
+	}
 }
 
 void Model::Clear()
@@ -28,8 +42,10 @@ void Model::Clear()
 
 void Model::Draw()
 {
-	glBegin(GL_TRIANGLES);
-	for (int i = 0; i < scene->mNumMeshes; i++) 
+	//glBegin(GL_TRIANGLES);
+
+	
+	/*for (int i = 0; i < scene->mNumMeshes; i++) 
 	{
 		aiMesh* mesh = scene->mMeshes[i];
 		for (int j = 0; j < mesh->mNumFaces; j++) 
@@ -43,6 +59,26 @@ void Model::Draw()
 				glVertex3f(pos.x, pos.y, pos.z);
 			}
 		}
+	}*/
+
+	for (int i = 0; i < scene->mNumMeshes; ++i)
+	{
+		aiMesh* mesh = scene->mMeshes[i];
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(mesh->mNumVertices, GL_FLOAT, 0, mesh->mVertices);
+		
+		glEnableClientState(GL_NORMAL_ARRAY);
+		glNormalPointer(GL_FLOAT, 0, mesh->mNormals);
+		
+		/*glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glTexCoordPointer(mesh->mNumUVComponents[0], GL_FLOAT, 0, mesh->mTextureCoords[0]);*/
+		
+		glDrawElements(GL_TRIANGLES, indices[i].size(), GL_UNSIGNED_INT, (void *) indices[i][0]);
+		
+	/*	glDisableClientState(GL_TEXTURE_COORD_ARRAY);*/
+		glDisableClientState(GL_NORMAL_ARRAY);
+		glDisableClientState(GL_VERTEX_ARRAY);
 	}
-	glEnd();
+
+	//glEnd();
 }
