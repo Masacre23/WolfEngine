66ad95d18cc0faa6cfcf1076c3253f152832ddc6
GuillemFP <guillemferre@gmail.com>
2017-02-15 01:21:23 +0100

    Added cube vertex normals and solved problem with drawing after the plane lines.

--- WolfEngine/Globals.h
+++ WolfEngine/Globals.h
@@ -21,20 +21,6 @@ enum update_status
 	UPDATE_ERROR
 };
 
-struct Point3d
-{
-	int x = 0, y = 0, z = 0;
-
-	Point3d(int x = 0, int y = 0, int z = 0) : x(x), y(y), z(z) {}
-};
-
-struct Point3df
-{
-	float x = 0.0f, y = 0.0f, z = 0.0f;
-
-	Point3df(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {}
-};
-
 // Deletes a buffer
 #define RELEASE( x ) \
     {									  \
