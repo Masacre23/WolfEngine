35a68509932a16874a53979e805872f1b7f48e52
Masacre23 <adrianguevaralonso@gmail.com>
2017-02-16 21:39:28 +0100

    Created a console window

--- WolfEngine/ModuleEditor.cpp
+++ WolfEngine/ModuleEditor.cpp
@@ -1,13 +1,13 @@
 #include "ModuleEditor.h"
 #include "Application.h"
 
-#include "Imgui/imgui.h"
-#include "Imgui/imgui_impl_sdl_gl3.h"
+
 #include <stdio.h>
 #include "Glew/include/GL/glew.h"   
 #include "SDL\include\SDL.h"
 
 #include "ModuleWindow.h"
+//#include "ModuleInput.h"
 
 ModuleEditor::ModuleEditor() : Module("ModuleEditor", true)
 {
@@ -37,17 +37,24 @@ update_status ModuleEditor::PreUpdate(float dt)
 
 update_status ModuleEditor::Update(float dt)
 {
-	bool show_test_window = true;
+	//bool show_test_window = true;
 	bool show_another_window = false;
-	LOG("pene");
 	ImGui_ImplSdlGL3_NewFrame(App->window->GetWindow());
 
 	static float f = 0.0f;
-	ImGui::Text("Hello, world!");
+	//ImGui::Text("Hello, world!");
 	/*ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
 	if (ImGui::Button("Test Window")) show_test_window ^= 1;
 	if (ImGui::Button("Another Window")) show_another_window ^= 1;
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);*/
+
+	//ImGui::ShowTestWindow();
+	/*for (int i = 0; i <= 100; ++i)
+	{
+		if (App->input->GetKey(i) == KEY_DOWN)
+			AddLog("");
+	}*/
+	Draw("Console");
 
 	ImGui::Render();
 
@@ -63,4 +70,23 @@ bool ModuleEditor::CleanUp()
 {
 	ImGui_ImplSdlGL3_Shutdown();
 	return true;
+}
+
+void ModuleEditor::Draw(const char* title, bool* p_opened)
+{
+	ImGui::Begin(title, p_opened);
+	ImGui::TextUnformatted(Buf.begin());
+	if (ScrollToBottom)
+		ImGui::SetScrollHere(1.0f);
+	ScrollToBottom = false;
+	ImGui::End();
+}
+
+void ModuleEditor::AddLog(const char* fmt, ...) IM_PRINTFARGS(2)
+{
+	va_list args;
+	va_start(args, fmt);
+	Buf.appendv(fmt, args);
+	va_end(args);
+	ScrollToBottom = true;
 }
--- WolfEngine/ModuleEditor.h
+++ WolfEngine/ModuleEditor.h
@@ -2,6 +2,8 @@
 #define MODULEEDITOR_H
 
 #include "Module.h"
+#include "Imgui/imgui.h"
+#include "Imgui/imgui_impl_sdl_gl3.h"
 
 class ModuleEditor : public Module
 {
@@ -15,6 +17,12 @@ public:
 	update_status Update(float dt);
 	update_status PostUpdate(float dt);
 	bool CleanUp();
+
+	void Draw(const char* title, bool* p_opened = NULL);
+	void AddLog(const char* fmt, ...);
+public:
+	ImGuiTextBuffer Buf;
+	bool ScrollToBottom = false;
 };
 
 #endif
