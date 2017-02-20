6bad3b4d464081440abef4b9c332c4a4a424c07a
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-02-20 16:00:09 +0100

    Created half of application config window

--- WolfEngine/Log.cpp
+++ WolfEngine/Log.cpp
@@ -8,6 +8,7 @@ void log(const char file[], int line, const char* format, ...)
 {
 	static char tmp_string[4096];
 	static char tmp_string2[4096];
+	static char tmp_string_editor[4096];
 	static va_list  ap;
 
 	// Construct the string from variable arguments
@@ -15,8 +16,9 @@ void log(const char file[], int line, const char* format, ...)
 	vsprintf_s(tmp_string, 4096, format, ap);
 	va_end(ap);
 	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
+	sprintf_s(tmp_string_editor, 4096, "%s\n", tmp_string);
 	OutputDebugString(tmp_string2);
 
 	if(App != nullptr)
-		App->editor->AddLog(tmp_string2);
+		App->editor->AddLog(tmp_string_editor);
 }
