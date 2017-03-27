9b6984a1fa87618de07b67e8e0b4f8cc70abfbf8
estebanag <estarr22@hotmail.com>
2017-03-27 20:44:08 +0200

    Add Billboard class.

---
+++ WolfEngine/Billboard.h
@@ -0,0 +1,27 @@
+#ifndef BILLBOARD_H
+#define BILLBOARD_H
+
+#include "Math.h"
+#include "assimp/types.h"
+
+class Billboard
+{
+
+public:
+	Billboard(aiString& texture, float3 position, float up, float right);
+	~Billboard();
+
+	void ComputeQuad(float3 camera);
+	void Draw();
+
+private:
+	const float3 up_vector = float3(0.0f, 1.0f, 0.0f);
+	unsigned int texture;
+	float3 vertices[4];
+	float3 position;
+	float up, right;
+
+};
+
+#endif // !BILLBOARD_H
+
