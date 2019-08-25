a8e884462e258806ccb276148f266a6da863e331
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2019-08-25 11:38:34 +0200

    Imgui stuff

--- WolfEngine/PanelInterface.cpp
+++ WolfEngine/PanelInterface.cpp
@@ -6,6 +6,13 @@
 #include "PanelHierachy.h"
 #include "ComponentTransform.h"
 
+//For project window
+#include <Windows.h>
+//#include <experimental/filesystem>
+//#include <filesystem>
+
+//namespace fs = std::experimental::filesystem;
+
 PanelInterface::PanelInterface(bool active) : Panel("Interface", active)
 {
 	hierachy = new PanelHierachy();
@@ -63,5 +70,28 @@ void PanelInterface::Draw()
 	ImGui::SetNextWindowPos(ImVec2(0, App->window->GetScreenHeight() - App->window->GetScreenHeight() / 3));
 	ImGui::SetNextWindowSize(ImVec2(App->window->GetScreenWidth(), App->window->GetScreenHeight() / 3));
 	ImGui::Begin("Project", &b, ImVec2(App->window->GetScreenWidth(), App->window->GetScreenHeight() / 3), -1.0f, ImGuiWindowFlags_ChildWindowAutoFitX | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_ChildWindowAutoFitY | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_ShowBorders);
+	std::string path = "";
+	/*for (auto & p : fs::directory_iterator(path))
+	{
+		APPLOG((char*)&p);
+		ImGui::Text((char*)&p);
+	}*/
+	//auto & p = fs::directory_iterator(path);
+	//APPLOG((char*)&p);
+	//ImGui::Text((char*)&p);
+	std::string pattern("Resources");
+	//stringvec v;
+	pattern.append("\\*");
+	WIN32_FIND_DATA data;
+	HANDLE hFind;
+	if ((hFind = FindFirstFile(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE)
+	{
+		do {
+			//APPLOG(data.cFileName);
+			ImGui::Text(data.cFileName);
+		} while (FindNextFile(hFind, &data) != 0);
+		FindClose(hFind);
+	}
+
 	ImGui::End();
 }
